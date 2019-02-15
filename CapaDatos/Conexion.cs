21fab0b07a14ce44dc5cf6cf74d556d0c0c474c3
Julian Mahecha <jemahechaj@ut.edu.co>
2019-02-15 11:33:35 -0500

    Creacion cadena de conexion

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string Cn = "Data Source:DESKTOP-MGVC88J; Initial Catalog:dbventas; Integrated Security=true"; //Cadena de conexion
+    }
+}
